ff375c0f9fc7d080ac500ed43c7796e740e3ae9d
pengweibiao <pengweibiao@echatsoft.com>
2022-08-15 22:11:00 +0800

    添加 加密工具 功能

--- src/views/AESCode/api/index.ts
+++ src/views/AESCode/api/index.ts
@@ -6,38 +6,7 @@ let paramsSerializer = function (p: any) {
   return qs.stringify(p, { arrayFormat: 'repeat' })
 }
 export default {
-  // 获取数据
-  getDataList(params:any,data:any){
-    const pageNum:number = params.pageNum
-    const pageSize:number = params.pageSize
-    let pathParams:any
-    if(params.startDate&&params.endDate){
-      pathParams = {
-        startDate:params.startDate,
-        endDate:params.endDate
-      }
-    }
-    return request.get(flag + `/extension/reqinfo/list/${pageNum}/${pageSize}`,{})(pathParams?pathParams:data)
-  },
-  // 一洽配置-查询所有数据
-  getEchatConfigDataList(){
-    return request.get(`/extension/companyconfig/list/1/1000`,{})(null)
-  },
-  //一洽配置-根据id查看详情
-  getEchatConfigDataByCompId(compId:string){
-    return request.get(flag + `/extension/system/config/echat/${compId}`,{})(compId)
-  },
-  //一洽配置-新增公司id
-  addEchatConfigDataByCompId(val: any){
-    return request.post(flag + `/extension/system/config/echat/compid`,{})(val)
-  },
-  //一洽配置-更新数据
-  updateEchatConfigDataByCompId(val: any){
-    return request.put(`/extension/companyconfig`,{})(val)
-  },
-  //一洽配置-更新数据
-  deleteEchatConfigDataByCompId(val: any){
-    return request.delete(flag + `/extension/system/config/echat`,{})(val)
-  },
-  // getDataList: request.get(flag + '/extension/reqinfo/list/{pageNum}/{pageSize}', {})
+  getDecode(param: any) {
+    return request.post('/extension/api/encodeJson', {})(param);
+  }
 } 
--- src/views/AESCode/index.vue
+++ src/views/AESCode/index.vue
@@ -12,137 +12,32 @@
             size="medium"
         >
           <div style="font-size: 36px" class="main_title" >明文</div>
-          <el-form-item label="公司名称" prop="companyName">
-            <el-input type="textarea" :rows="8" v-model="form.companyName" clearable></el-input>
-          </el-form-item>
-          <el-form-item label="appid" prop="appid">
-            <el-input v-model="form.appid" clearable></el-input>
-          </el-form-item>
-          <el-form-item label="aesKey" prop="aesKey">
-            <el-input v-model="form.aesKey" clearable></el-input>
-          </el-form-item>
-          <el-form-item label="token" prop="token">
-            <el-input v-model="form.token" clearable></el-input>
-          </el-form-item>
-          <div style="font-size: 24px" class="main_title">会员数据(metaData)</div>
-          <el-form-item label="会员的唯一值" prop="uid">
-            <el-input v-model="metaData.uid" clearable></el-input>
-          </el-form-item>
-          <el-form-item label="会员级别" prop="grade">
-            <el-input v-model="metaData.grade" clearable></el-input>
-          </el-form-item>
-          <el-form-item label="会员类别" prop="category">
-            <el-input v-model="metaData.category" clearable></el-input>
-          </el-form-item>
-          <el-form-item label="会员姓名" prop="name">
-            <el-input v-model="metaData.name" clearable></el-input>
-          </el-form-item>
-          <el-form-item label="会员昵称" prop="nickName">
-            <el-input v-model="metaData.nickName" clearable></el-input>
-          </el-form-item>
-          <el-form-item label="会员性别" prop="gender">
-            <el-input v-model="metaData.gender" clearable></el-input>
-          </el-form-item>
-          <el-form-item label="会员年龄" prop="age">
-            <el-input v-model="metaData.age" clearable></el-input>
-          </el-form-item>
-          <el-form-item label="会员生日" prop="birthday">
-            <el-input v-model="metaData.birthday" clearable></el-input>
-          </el-form-item>
-          <el-form-item label="会员婚姻状况" prop="maritalStatus">
-            <el-input v-model="metaData.maritalStatus" clearable></el-input>
-          </el-form-item>
-          <el-form-item label="会员联系电话" prop="phone">
-            <el-input v-model="metaData.phone" clearable></el-input>
-          </el-form-item>
-          <el-form-item label="会员QQ" prop="qq">
-            <el-input v-model="metaData.qq" clearable></el-input>
-          </el-form-item>
-          <el-form-item label="会员微信" prop="wechat">
-            <el-input v-model="metaData.wechat" clearable></el-input>
-          </el-form-item>
-          <el-form-item label="会员邮件地址" prop="email">
-            <el-input v-model="metaData.email" clearable></el-input>
-          </el-form-item>
-          <el-form-item label="会员国家" prop="nation">
-            <el-input v-model="metaData.nation" clearable></el-input>
-          </el-form-item>
-          <el-form-item label="会员省份" prop="province">
-            <el-input v-model="metaData.province" clearable></el-input>
-          </el-form-item>
-          <el-form-item label="会员城市" prop="city">
-            <el-input v-model="metaData.city" clearable></el-input>
-          </el-form-item>
-          <el-form-item label="会员地址" prop="address">
-            <el-input v-model="metaData.address" clearable></el-input>
-          </el-form-item>
-          <el-form-item label="会员头像地址" prop="photo">
-            <el-input v-model="metaData.photo" clearable></el-input>
-          </el-form-item>
-          <el-form-item label="会员备注信息" prop="memo">
-            <el-input v-model="metaData.memo" clearable></el-input>
-          </el-form-item>
-          <el-form-item label="会员自定义字段1" prop="c1">
-            <el-input v-model="metaData.c1" clearable></el-input>
-          </el-form-item>
-          <el-form-item label="会员自定义字段2" prop="c2">
-            <el-input v-model="metaData.c2" clearable></el-input>
-          </el-form-item>
-          <el-form-item label="会员自定义字段3" prop="c3">
-            <el-input v-model="metaData.c3" clearable></el-input>
-          </el-form-item>
-          <el-form-item label="会员自定义字段4" prop="c4">
-            <el-input v-model="metaData.c4" clearable></el-input>
-          </el-form-item>
-          <el-form-item label="会员自定义字段5" prop="c5">
-            <el-input v-model="metaData.c5" clearable></el-input>
-          </el-form-item>
-          <el-form-item label="会员自定义字段6" prop="c6">
-            <el-input v-model="metaData.c6" clearable></el-input>
-          </el-form-item>
-          <el-form-item label="会员自定义字段7" prop="c7">
-            <el-input v-model="metaData.c7" clearable></el-input>
-          </el-form-item>
-          <el-form-item label="会员自定义字段8" prop="c8">
-            <el-input v-model="metaData.c8" clearable></el-input>
-          </el-form-item>
-          <el-form-item label="会员自定义字段9" prop="c9">
-            <el-input v-model="metaData.c9" clearable></el-input>
-          </el-form-item>
-          <el-form-item label="会员自定义字段10" prop="c10">
-            <el-input v-model="metaData.c10" clearable></el-input>
-          </el-form-item>
-          <el-form-item label="会员自定义字段11" prop="c11">
-            <el-input v-model="metaData.c11" clearable></el-input>
-          </el-form-item>
-          <el-form-item label="会员自定义字段12" prop="c12">
-            <el-input v-model="metaData.c12" clearable></el-input>
-          </el-form-item>
-          <el-form-item label="会员自定义字段13" prop="c13">
-            <el-input v-model="metaData.c13" clearable></el-input>
-          </el-form-item>
-          <el-form-item label="会员自定义字段14" prop="c14">
-            <el-input v-model="metaData.c14" clearable></el-input>
-          </el-form-item>
-          <el-form-item label="会员自定义字段15" prop="c15">
-            <el-input v-model="metaData.c15" clearable></el-input>
-          </el-form-item>
-          <el-form-item label="会员自定义字段16" prop="c16">
-            <el-input v-model="metaData.c16" clearable></el-input>
-          </el-form-item>
-          <el-form-item label="会员自定义字段17" prop="c17">
-            <el-input v-model="metaData.c17" clearable></el-input>
-          </el-form-item>
-          <el-form-item label="会员自定义字段18" prop="c18">
-            <el-input v-model="metaData.c18" clearable></el-input>
-          </el-form-item>
-          <el-form-item label="会员自定义字段19" prop="c19">
-            <el-input v-model="metaData.c19" clearable></el-input>
-          </el-form-item>
-          <el-form-item label="会员自定义字段20" prop="c20">
-            <el-input v-model="metaData.c20" clearable></el-input>
+          <el-form-item label="json" prop="plainText">
+            <el-input type="textarea" :rows="8" v-model="form.plainText" clearable></el-input>
           </el-form-item>
         </el-form>
+        <div style="width: 600px" class="btn-warp">
+          <el-button @click="save" type="success">加密</el-button>
+          <el-button @click="del" type="danger">清空</el-button>
+        </div>
+<!--        <el-form-->
+<!--            ref="configForm"-->
+<!--            :model="decodeForm"-->
+<!--            :rules="rules"-->
+<!--            label-width="160px"-->
+<!--            label-position="right"-->
+<!--            label-suffix="："-->
+<!--            size="medium"-->
+<!--        >-->
+<!--          <div style="font-size: 36px" class="main_title" >密文</div>-->
+<!--          <el-form-item label="json" prop="plainText">-->
+<!--            <el-input type="textarea" :rows="8" v-model="decodeForm.plainText" clearable></el-input>-->
+<!--          </el-form-item>-->
+<!--        </el-form>-->
+<!--        <div style="width: 600px" class="btn-warp">-->
+<!--          <el-button @click="save" type="success">加密</el-button>-->
+<!--          <el-button @click="del" type="danger">清空</el-button>-->
+<!--        </div>-->
       </el-col>
       <el-col :offset="1" :span="15" style="height: 100%" class="rightBg">
         <div style="font-size: 36px" class="main_title">密文</div>
@@ -151,39 +46,11 @@
             <el-card class="box-card">
               <template #header>
                 <div class="card-header">
-                  <span>解密</span>
+                  <span>json</span>
                 </div>
               </template>
-              <div>{{ form.companyId }}</div>
+              <div  style="white-space: pre-wrap;" >{{ form.encrypt }}</div>
             </el-card>
-            <el-card class="box-card">
-              <template #header>
-                <div class="card-header">
-                  <span>Card name</span>
-                </div>
-              </template>
-              <div v-for="o in 4" :key="o" class="text item">{{ 'List item ' + o }}</div>
-            </el-card>
-            <el-card class="box-card">
-              <template #header>
-                <div class="card-header">
-                  <span>Card name</span>
-                </div>
-              </template>
-              <div v-for="o in 4" :key="o" class="text item">{{ 'List item ' + o }}</div>
-            </el-card>
-            <el-card class="box-card">
-              <template #header>
-                <div class="card-header">
-                  <span>Card name</span>
-                </div>
-              </template>
-              <div v-for="o in 4" :key="o" class="text item">{{ 'List item ' + o }}</div>
-            </el-card>
-            <div style="width: 600px" class="btn-warp">
-              <el-button @click="save" type="success">加密</el-button>
-              <el-button @click="del" type="danger">清空</el-button>
-            </div>
           </div>
         </template>
       </el-col>
@@ -202,11 +69,12 @@ export default class DataList extends Vue {
   private data: any = []
   private list: any = []
   private form: any = {
-    companyId: '',
-    companyName: '',
-    appid: '',
-    aesKey: '',
-    token: ''
+    encrypt: '',
+    plainText:''
+  }
+  private decodeForm: any = {
+    encrypt: '',
+    plainText:''
   }
   private metaData: any = {
     uid: '',
@@ -251,18 +119,9 @@ export default class DataList extends Vue {
   }
 
   private rules: any = {
-    appid: [
-      { required: true, message: 'appId不能为空', trigger:'blur'}
+    plainText: [
+      { required: true, message: '明文不能为空', trigger:'blur'}
     ],
-    companyName: [
-      { required: true, message: '公司名称不能为空', trigger:'blur'}
-    ],
-    aesKey: [
-      { required: true, message: 'aesKey不能为空', trigger:'blur'}
-    ],
-    token: [
-      { required: true, message: 'token不能为空', trigger:'blur'}
-    ]
   }
   private created() {
     this.requestData()
@@ -298,28 +157,9 @@ export default class DataList extends Vue {
    * 更新操作
    * @private
    */
-  private save() {
-    // 获取表单对象
-    const form = this.form;
-    // 表单校验
-    this.$refs.configForm.validate(async (valid: boolean) => {
-      if (valid) {
-        // 调用更新接口
-        const result: any = await api.updateEchatConfigDataByCompId(form);
-        if (result.code == 200) {
-          this.$message({
-            type:'success',
-            message:'保存成功'
-          });
-          this.requestData();
-        } else {
-          this.$message({
-            type:'error',
-            message:'保存失败'
-          });
-        }
-      }
-    });
+  private async save() {
+    const result: any = await api.getDecode(this.form.plainText);
+    this.form.encrypt = result.data;
   }
 
   /**
@@ -365,39 +205,9 @@ export default class DataList extends Vue {
    * @private
    */
   private async del() {
-    // 校验删除信息
-    if (this.form.compId == undefined || this.form.compId == '') {
-      this.$message({
-        type:'error',
-        message:'compId不存在'
-      });
-      return;
-    }
-
-    const compId: string = this.form.compId;
-    // 弹框确认是否执行删除操作
-    this.$confirm('此操作将删除账号'+ compId +'公司的所有配置', '提示',{
-      confirmButtonText: '删除',
-      cancelButtonText: '取消',
-      type: 'warning'
-    }).then(async () => {
-      // 删除数据
-      const result: any = await api.deleteEchatConfigDataByCompId(this.form);
-      if (result.code == 200) {
-        this.$message({
-          type:'success',
-          message:'删除成功'
-        });
-      }
-      // 刷新公司id列表
-      this.requestData();
       // 刷新公司配置
-      this.form.companyId = '';
-      this.form.appid = '';
-      this.form.companyName = '';
-      this.form.aesKey = '';
-      this.form.token = '';
-    });
+      this.form.encrypt = '';
+      this.form.plainText = '';
   }
 }
 
